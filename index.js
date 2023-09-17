2f84d34c89fb6f1dc64690b8a1fa379c56bbbba6
Justin Higgins <justinhiggy@gmail.com>
2023-09-17 15:33:01 -0700

    sImPlEr oRgAnIzAtIoN

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 module.exports = {
   rules: {
-    'enforce-variable-naming': require('./rules/enforce-variable-naming'),
+    'sPoNgEbOb': {
+      meta: {
+        type: 'suggestion',
+        docs: {
+          description: 'Enforce variable naming in the format sPoNgEbOb...',
+        },
+      },
+      create: function (context) {
+        return {
+          Identifier: function (node) {
+            const name = node.name;
+
+            // Check if the variable name matches the pattern.
+            if (!/^[a-z]([A-Z][a-z])*[A-Z]?$/.test(name)) {
+              context.report({
+                node,
+                message: 'Variable name is not in sPoNgEbObCaSe...',
+              });
+            }
+          },
+        };
+      },
+    }
   },
 };
